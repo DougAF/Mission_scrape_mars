babd5a9ee7a135b29129645b9f0b27dc5c8d27ef
DougAF <31972099+DougAF@users.noreply.github.com>
2019-04-18 22:48:02 -0500

    Notebook cleaned, app.py needs index and scrape route

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 # imports
-from flask import Flask, current_app, jsonify, redirect, render_template
-#from flask_pymongo import PyMongo
+from flask import Flask, current_app, jsonify, render_template
+from flask_pymongo import PyMongo
 import scrape_mars
 
 #Flask instance
@@ -8,28 +8,16 @@ app = Flask(__name__)
 #link to DB
 mongo = PyMongo(app)
 
-# create index route
-@app.route("/")
-def index():
-    scrape_mars_data = mongo.db.mars.find_one()
-    if bool(mars_data):
-        return render_template("index.html", mars_dict=mars_data)
-    else:
-        return current_app.send_static_file('index.html')
+# # create index 
+# @app.route("/")
+# def index():
+    
+#     else:
+#         return current_app.send_static_file('index.html')
 
-# create scrape route
-@app.route("/scrape")
-def scrape():
+# create scrape 
+# @app.route("/scrape")
+# def scrape():
 
-    mars_dict = mongo.db.mars
-    mars_data = scrape_mars.scrape()
-    mars_dict.update(
-        {},
-        mars_data,
-        upsert=True
-    )
-    return redirect("http://localhost:5000/", code=302)
-
-
-if __name__ == "__main__":
-    app.run(debug=True)
+# if __name__ == "__main__":
+#     app.run(debug=True)
