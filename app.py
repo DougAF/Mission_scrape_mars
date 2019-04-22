dfe1546312eeec6834d0a69b63ffcc6976fce84a
DougAF <31972099+DougAF@users.noreply.github.com>
2019-04-22 18:34:23 -0500

    conda/Flask_PyMongo installation issues- some debugguing complete, more work needed

--- app.py
+++ app.py
@@ -1,23 +1,35 @@
 # imports
-from flask import Flask, current_app, jsonify, render_template
+from flask import Flask, current_app, jsonify, render_template, redirect
+
 from flask_pymongo import PyMongo
+
 import scrape_mars
 
 #Flask instance
 app = Flask(__name__)
-#link to DB
-mongo = PyMongo(app)
+# Create connection variable
+mongo = PyMongo(app, uri="mongodb://localhost:27017")
 
-# # create index 
-# @app.route("/")
-# def index():
-    
-#     else:
-#         return current_app.send_static_file('index.html')
+# create index 
+@app.route("/")
+def index():
+#         # Store the entire dict collection in a dict  dict(db.mars_data.find())
+    mars_app_dict = mongo.db.collection.find_one()
+    print("scrape_mars_dict")
+    return render_template('index.html', mars_data_dict= mars_app_dict)
+# #         return current_app.send_static_file('index.html')
 
 # create scrape 
-# @app.route("/scrape")
-# def scrape():
+@app.route("/scrape")
+def scrape():
+    scrape_mars_dict = scrape_mars.scrape()
+    mars_data = mongo.db.mars
+    mars_data.update(
+        {},
+        scrape_mars_dict,
+        upsert=True
+    )
+    return redirect("http://localhost:5000/")
 
-# if __name__ == "__main__":
-#     app.run(debug=True)
+if __name__ == "__main__":
+    app.run(debug=True)
